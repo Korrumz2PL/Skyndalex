fd03c29205cb24183021ede0d3997004d2b800df
entity <55253305+Korrumz2PL@users.noreply.github.com>
2021-08-12 23:07:04 +0200

    Delete src directory

--- src/commands/bot/ping.js
+++
@@ -1,9 +0,0 @@
-exports.help = {
-    async execute(interaction) {
-		await interaction.reply(`Ping: ${client.ws.ping}`);
-	},
-    name: "ping",
-    description: "Ping bota",
-    category: "bot",
-    aliases: ["pong"]
-}
